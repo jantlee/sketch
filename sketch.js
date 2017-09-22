fa9b0560b7baa830c8f93f141b8d9654c1f7f209
Jay Anthony <jantleej@gmail.com>
2017-09-21 22:25:14 -0500

    Implemented prompt for grid sizing. Added Bubble Mode, Reset, and size changing functionality

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-    var inputNum = 50;
-    var cellSize = 500 / inputNum;
-    var gridCol = '';
+    var inputNum = 16; //Eventually allow user-input via prompt
+    var cellSize = $('.container').height() / inputNum;
+    var gridCol;
+    var bubbleMode;
+    var gridMode;
 
     function setGridColumns() {
+        gridCol = '';
         for (var i = 0; i < inputNum; i++) {
             gridCol += cellSize + 'px ';
         }
@@ -14,6 +17,10 @@ $(document).ready(function() {
         for (var i = 0; i < inputNum ** 2; i++) {
             $('.pad').append('<div class="pad-pixel"></div>');
         }
+
+        if (bubbleMode == true) {
+            activateBubble();
+        }
     }
 
     function draw() {
@@ -22,15 +29,45 @@ $(document).ready(function() {
         });
     }
 
-    function clear() {
+    function promptForSize() {
+        var size = prompt('What size would you like the grid to be?');
+        inputNum = size;
+        cellSize = $('.container').height() / inputNum;
 
+    }
+
+    function activateGrid() {
+        bubbleMode = false;
+        gridMode = true;
+        $('.pad-pixel').css('border-radius', '0%');
+    }
+
+    function activateBubble() {
+        bubbleMode = true;
+        gridMode = false;
+        $('.pad-pixel').css('border-radius', '100%');
+    }
+
+    function resetGrid() {
+        $('.pad').empty();
+        setGridColumns();
+        setGridCells();
+        draw();
     }
 
     setGridColumns();
     setGridCells();
-    draw()
+    draw();
 
-    $('#clear').on('click', function() {
-        $('.pad-pixel').css('background-color', 'white');
+    $('#change-size').on('click', function() {
+        promptForSize();
+        resetGrid();
     });
+
+    $('#reset').on('click', function() {
+        resetGrid();
+    });
+
+    $('#grid').on('click', activateGrid);
+    $('#bubble').on('click', activateBubble);
 });
